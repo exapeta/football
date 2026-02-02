60e5fae9c92f1c45037ec3d808034bbbd2464f6b
e <e@modulus.local>
2026-02-02 13:07:28 +0000

    Update tests again

--- tests/test_football.py
+++ tests/test_football.py
@@ -1,9 +1,11 @@
-import pytest
 import sys
 import os
 
 # Ensure the parent directory is in the sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from football import app  # Importing the app variable from football.py
+import pytest
 
 @pytest.fixture
 def client():
